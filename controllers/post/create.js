ef5d60e2bf18b368930a478555409964b47f7616
IKunDefined <1254729251@qq.com>
2019-04-23 15:39:29 +0800

    update

--- controllers/post/create.js
+++ controllers/post/create.js
@@ -1,12 +1,13 @@
 const Post = require('../../models/Post')
+const markdown = require('markdown').markdown
 
 module.exports = (req, res) => {
   let responseData = {
     code: 0,
     message: '发布成功'
   }
-  const { title, summary, content, createAt, tags, category } = req.body.post
-  console.log(category)
+  let { title, summary, content, createAt, tags, category } = req.body.post
+  content = markdown.toHTML(content)
   let post = new Post({
     title,
     summary,
